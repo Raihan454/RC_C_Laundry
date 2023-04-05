34ebd1ac2fbcc0a91f16296cabd1dceb49ce8a04
Raihan454 <raihanmarsa712@gmail.com>
2023-04-05 16:33:51 +0700

    Add project files.

---
+++ RC_C/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RC_C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+            while(true)
+        }
+    }
+}
